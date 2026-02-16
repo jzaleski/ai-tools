d6bedf6ea59257bb998a0359afcbb24de61923f5
Jonathan W. Zaleski <JonathanZaleski@gmail.com>
2026-02-16 17:05:37 -0500

    fix: Follow _Docker_ convention for interpolation + clean-up

--- bin/run-open-webui.sh
+++ bin/run-open-webui.sh
@@ -2,10 +2,9 @@
 
 set -e;
 
-ADVISOR_MODEL_PORT=${ADVISOR_MODEL_PORT:-8082};
-PORT=${PORT:-8080};
-
+ADVISOR_MODEL_PORT=${ADVISOR_MODEL_PORT:-"8082"};
 IMAGE=${IMAGE:-"ghcr.io/open-webui/open-webui:main"};
+PORT=${PORT:-"8080"};
 
 docker pull ${IMAGE} && \
   docker compose -f $(dirname $0)/../docker-compose-files/open-webui.yml up;
